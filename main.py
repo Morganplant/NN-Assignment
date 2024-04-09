db9e865436b1dc195591c9dc2e300e758580628d
Andris37 <57486647+Morganplant@users.noreply.github.com>
2024-04-09 20:11:44 +0100

    current

--- main.py
+++ main.py
@@ -1,65 +1,27 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
+import logging
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(f"Using {str(device).upper()}")
-
 import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torch
+from rich.logging import RichHandler
+from rich.progress import Progress, track
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets
 
-# Define transformations
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+FORMAT = "%(message)s"
+log = logging.basicConfig(
+    level="DEBUG", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
 
-# Load the CIFAR-10 training set
-trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_DIR = os.path.join(ROOT_DIR, "models")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
 
-# Split the training set into training and validation sets
-# Assuming 80% for training and 20% for validation
-train_size = int(0.8 * len(trainset))
-val_size = len(trainset) - train_size
-trainset, valset = torch.utils.data.random_split(trainset, [train_size, val_size])
-
-# Define batch size
-batch_size = 4
-
-# Create data loaders for training, validation, and test sets
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=2)
-testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-
-# Define classes
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-# functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-
-
-import torch.nn as nn
 
 class Net(nn.Module):
     def __init__(self):
@@ -80,98 +42,153 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-net = Net()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+def load_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    if os.path.exists("./data"):
+        trainloader, valloader, testloader, classes = torch.load(DATA_DIR)
+        return trainloader, valloader, testloader, classes
+    os.mkdir("./data")
+    trainset = datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
+    train_size = int(0.8 * len(trainset))
+    val_size = len(trainset) - train_size
+    trainset, valset = random_split(trainset, [train_size, val_size])
+
+    batch_size = 4
+
+    trainloader = DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+    )
+    valloader = DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=2)
+    testset = datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform
+    )
+    testloader = DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=2
+    )
+
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
+
+    return trainloader, valloader, testloader, classes
 
 
-import torch
-import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from tqdm import tqdm
+def train_model(trainloader, valloader, testloader):
+    net = Net()
+    logging.info("Starting Training")
+    epoch_limit = 50
 
-# Assuming you have a validation loader named 'valloader'
-# Assuming 'net', 'criterion', and 'optimizer' are defined elsewhere
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-loss_values = []
+    loss_values = []
+    best_val_loss = float("inf")
+    prev_val_loss = float("inf")
+    patience = 5
+    wait = 0
 
-best_val_loss = float('inf')
-prev_val_loss = float('inf')
-patience = 5  # Number of epochs to wait before reducing learning rate
-wait = 0
+    with Progress() as progress:
+        training_progress = len(trainloader) * epoch_limit
+        validation_progress = len(valloader) * epoch_limit
+        analysis_progress = len(testloader) * epoch_limit
 
-epoch_limit = 50
+        training = progress.add_task("[red]Training...", total=training_progress)
+        validation = progress.add_task(
+            "[green]Validation...", total=validation_progress
+        )
+        analysis = progress.add_task("[blue]Analysis...", total=analysis_progress)
 
-# Set the number of workers for data loading
-num_workers = 4  # Adjust this number based on your CPU's capacity
-
-for epoch in tqdm(range(50)):  # Number of maximum epochs
-    net.train()
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):  # enumerate over trainloader
-        inputs, labels = data  # unpack data
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i % 2000 == 1999:
-            loss_values.append(round((running_loss / 2000), 3))
+        for epoch in range(epoch_limit):
+            net.train()
             running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                progress.update(training, advance=1)
+                inputs, labels = data
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-    # Validation loss calculation
-    net.eval()
-    val_loss = 0.0
-    with torch.no_grad():
-        for data in valloader:
-            inputs, labels = data
-            outputs = net(inputs)
-            val_loss += criterion(outputs, labels).item()
-    val_loss /= len(valloader)
+                running_loss += loss.item()
+                if i % 2000 == 1999:
+                    loss_values.append(round((running_loss / 2000), 3))
+                    running_loss = 0.0
 
-    # Check for early stopping condition
-    if val_loss > prev_val_loss:
-        wait += 1
-        if wait >= patience:
-            print(f'Early stopping at epoch {epoch + 1} due to validation loss increase or plateauing.')
-            break
-    else:
-        wait = 0
+            net.eval()
+            val_loss = 0.0
+            with torch.no_grad():
+                for data in valloader:
+                    progress.update(validation, advance=1)
+                    inputs, labels = data
+                    outputs = net(inputs)
+                    val_loss += criterion(outputs, labels).item()
+            val_loss /= len(valloader)
 
-    prev_val_loss = val_loss
+            if val_loss > prev_val_loss and save_count > 0:
+                wait += 1
+                if wait >= patience:
+                    logging.debug(
+                        f"Saving Current Best Model: {prev_val_loss} - {prev_val_loss}"
+                    )
+                    torch.save(net.state_dict(), f"{MODEL_DIR}/best_model.pt")
+                    save_count = +1
+            else:
+                wait = 0
 
-    # Save the model if validation loss improved
-    if val_loss < best_val_loss:
-        torch.save(net.state_dict(), 'model/best_model.pt')
-        best_val_loss = val_loss
+            prev_val_loss = val_loss
 
-print('Finished Training')
+        logging.info("Finished Training")
+
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                progress.update(analysis, advance=1)
+                images, labels = data
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        logging.debug(
+            f"Accuracy of the network on the 10000 test images: {100 * correct // total} % - [{correct}/{total}]"
+        )
+
+    return loss_values
 
 
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+def plot_training_loss(loss_values):
+    import plotly.graph_objects as go
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} % - [{correct}/{total}]')
+    fig = go.Figure(data=go.Scatter(x=list(range(len(loss_values))), y=loss_values))
+    fig.update_layout(
+        title="Training Loss",
+        xaxis_title="Epoch",
+        yaxis_title="Loss",
+    )
+    fig.show()
 
-import plotly.graph_objects as go
-fig = go.Figure(
-    data=go.Scatter(x=list(range(len(loss_values))), y=loss_values)
-)
-fig.update_layout(
-    title="Training Loss",
-    xaxis_title="Epoch",
-    yaxis_title="Loss",
-)
+
+def main():
+    trainloader, valloader, testloader, classes = load_data()
+    loss_values = train_model(trainloader, valloader, testloader)
+    plot_training_loss(loss_values)
+
+
+if __name__ == "__main__":
+    main()
